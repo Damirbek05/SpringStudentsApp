89001a13111b0b145768f4a7f92613c328284ee2
Damir Ospanbek <ospanbekdamir1@gmail.com>
2024-06-20 13:07:48 +0600

    Repository object added

--- src/main/java/by/damir/springstudentsapp/Services/Impl/InMemoryStudentServiceImpl.java
+++ src/main/java/by/damir/springstudentsapp/Services/Impl/InMemoryStudentServiceImpl.java
@@ -1,37 +1,41 @@
 package by.damir.springstudentsapp.Services.Impl;
 
 import by.damir.springstudentsapp.Models.Student;
+import by.damir.springstudentsapp.Repository.InMemoryStudentDAO;
 import by.damir.springstudentsapp.Services.StudentService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class InMemoryStudentServiceImpl implements StudentService {
 
+    private final InMemoryStudentDAO repository;
 
     @Override
     public List<Student> findAllStudent() {
-        return null;
+        return repository.findAllStudent();
     }
 
     @Override
     public Student saveStudent(Student student) {
-        return null;
+        return repository.saveStudent(student);
     }
 
     @Override
     public Student findByEmail(String email) {
-        return null;
+        return repository.findByEmail(email);
     }
 
     @Override
     public Student updateStudent(Student student) {
-        return null;
+        return repository.updateStudent(student);
     }
 
     @Override
     public void deleteStudent(String email) {
-        return null;
+        repository.deleteStudent(email);
     }
 }
