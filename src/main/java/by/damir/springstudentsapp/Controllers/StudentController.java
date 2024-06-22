12b02652aefd6892ae70dca5143421bd24067250
Damir Ospanbek <ospanbekdamir1@gmail.com>
2024-06-22 23:42:56 +0600

    Controllers added

--- src/main/java/by/damir/springstudentsapp/Controllers/StudentController.java
+++ src/main/java/by/damir/springstudentsapp/Controllers/StudentController.java
@@ -4,11 +4,11 @@ package by.damir.springstudentsapp.Controllers;
 import by.damir.springstudentsapp.Models.Student;
 import by.damir.springstudentsapp.Services.StudentService;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,13 +16,33 @@ import java.util.List;
 @RequestMapping("/api/v1/students")
 @AllArgsConstructor
 public class StudentController {
-
-    private StudentService service;
+    @Qualifier("studentService")
+    private final StudentService service;
 
     @GetMapping
     public List<Student> findAllStudent(){
         //todo
         return service.findAllStudent();
+    }
+
+    @PostMapping("save_student")
+    public String saveStudent(@RequestBody Student student){
+        service.saveStudent(student);
+        return "Студент успешно сохранен";
+    }
+
+    @GetMapping("/{email}")
+    public Student findByEmail(@PathVariable String email){
+        return service.findByEmail(email);
+    }
+
+    @PutMapping("update_student")
+    public Student updateStudent(@RequestBody Student student){
+        return service.updateStudent(student);
+    }
+    @DeleteMapping("delete_student/{email}")
+    public void deleteStudent(@PathVariable String email){
+        service.deleteStudent(email);
     }
 
 }
