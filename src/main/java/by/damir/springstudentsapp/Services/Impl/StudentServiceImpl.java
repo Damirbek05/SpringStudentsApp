e158fad3c8025ed4c356e214b74280890ea457a5
Damir Ospanbek <ospanbekdamir1@gmail.com>
2024-07-18 00:06:33 +0600

    Adding StudentServiceImpl @Primary , @Transactional to method deleteStudent

--- src/main/java/by/damir/springstudentsapp/Services/Impl/StudentServiceImpl.java
+++ src/main/java/by/damir/springstudentsapp/Services/Impl/StudentServiceImpl.java
@@ -3,13 +3,16 @@ package by.damir.springstudentsapp.Services.Impl;
 import by.damir.springstudentsapp.Models.Student;
 import by.damir.springstudentsapp.Repository.StudentRepo;
 import by.damir.springstudentsapp.Services.StudentService;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Primary
 public class StudentServiceImpl implements StudentService {
     private final StudentRepo repository;
     @Override
@@ -24,16 +27,17 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public Student findByEmail(String email) {
-        return repository.findByEmail(email);
+        return repository.findStudentByEmail(email);
     }
 
     @Override
     public Student updateStudent(Student student) {
-        return repository.updateStudent(student);
+        return repository.save(student);
     }
 
     @Override
+    @Transactional
     public void deleteStudent(String email) {
-        repository.deleteStudent(email);
+        repository.deleteByEmail(email);
     }
 }
